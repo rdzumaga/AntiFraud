438af6abdc58b0a67c49b18b1c36488747d18950
Rafał Dżumaga <rafal@excodus.com>
2020-09-23 21:37:31 +0200

    added purchase controller

--- AntiFraud.API/Dto/PurchaseDto.cs
+++ AntiFraud.API/Dto/PurchaseDto.cs
@@ -1,11 +1,12 @@
 ﻿using AntiFraud.API.Models;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace AntiFraud.API.Dto
 {
-    public class PurchaseDto
+    public class PurchaseDto : IValidatableObject
     {
         [JsonProperty("email")]
         public string Email { get; set; }
@@ -25,6 +26,46 @@ namespace AntiFraud.API.Dto
         public Purchase ToDomainObject()
         {
             return new Purchase(Email, Amount, Currency, Address.ToDomainObject(), Products.Select(x => x.ToDomainObject()).ToList());
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var validationResult = new List<ValidationResult>();
+            if (string.IsNullOrWhiteSpace(Email)) validationResult.Add(new ValidationResult("Email cannot be empty", new[] { nameof(Email) }));
+            else
+            {
+                var emailValid = new EmailAddressAttribute().IsValid(Email);
+                if (!emailValid) validationResult.Add(new ValidationResult("Email is invalid", new[] { nameof(Email) }));
+            }
+
+            return validationResult;
+        }
+    }
+
+    public static class PurchaseExtensions
+    {
+        public static PurchaseDto ToDto(this Purchase purchase)
+        {
+            var dto = new PurchaseDto()
+            {
+                Email = purchase.Email,
+                Amount = purchase.Amount,
+                Currency = purchase.Currency,
+                Address = new AddressDto()
+                {
+                    City = purchase.Address.City,
+                    Country = purchase.Address.Country,
+                    Street = purchase.Address.Street,
+                    Zipcode = purchase.Address.Zipcode
+                },
+                Products = purchase.Products.Select(x => new ProductDto()
+                {
+                    Name = x.Name,
+                    Quantity = x.Quantity
+                }).ToList()
+            };
+
+            return dto;
         }
     }
 }
